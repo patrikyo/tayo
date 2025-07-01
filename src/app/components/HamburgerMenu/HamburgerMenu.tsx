78211381cdf234cae872ed1f6c43f5b82157c920
youpa <patrik.youssef@arbetsformedlingen.se>
2025-07-01 17:45:06 +0200

    Adds toggle functionality with accessibility

---
+++ src/app/components/HamburgerMenu/HamburgerMenu.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./HamburgerMenu.module.css";
+import Navigation from "../Navigation/Navigation";
+import HamburgerMenuProps from "@/app/models/interfaces/HamburgerMenuProps.interface";
+import { useState } from "react";
+
+const HamburgerMenu: React.FC<HamburgerMenuProps> = ({ links }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <>
+      <button
+        className={styles.toggleBtn}
+        aria-expanded={isMenuOpen}
+        aria-controls="hamburger-navigation"
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        {!isMenuOpen && (
+          <FontAwesomeIcon
+            icon={faBars}
+            size="3x"
+            color="#000000"
+            aria-hidden="true"
+          />
+        )}
+
+        {isMenuOpen && (
+          <FontAwesomeIcon
+            icon={faClose}
+            size="3x"
+            color="#000000"
+            aria-hidden="true"
+          />
+        )}
+      </button>
+
+      <div
+        id="hamburger-navigation"
+        className={`${
+          isMenuOpen ? styles.showNavigation : styles.hideNavigation
+        }`}
+      >
+        <Navigation links={links} />
+      </div>
+    </>
+  );
+};
+
+export default HamburgerMenu;
