239a61ba73d1c9d4b73d0bd8468e24c5398893e5
youpa <patrik.youssef@arbetsformedlingen.se>
2025-07-02 10:29:09 +0200

    Adds heroSection

---
+++ src/app/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,10 @@
+import styles from "./HeroSection.module.css";
+const HeroSection = () => {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Rulltrappor & Hissar</h2>
+    </div>
+  );
+};
+
+export default HeroSection;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./components/Header/Header";
+import HeroSection from "./components/HeroSection/HeroSection";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <HeroSection />
     </div>
   );
 }
